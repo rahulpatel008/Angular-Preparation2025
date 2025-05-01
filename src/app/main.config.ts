418faf86ffd57c8d5b0ccf92204f3786a5d2374a
Rahul Patel <rahulspatel008@yahoo.in>
2025-05-01 19:34:44 +0530

    NGRX Lib Added

--- src/app/main.config.ts
+++ src/app/main.config.ts
@@ -1,13 +1,18 @@
 import { provideHttpClient } from "@angular/common/http";
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { routes } from "./main.routes";
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const mainConfig: ApplicationConfig = {
     providers: [
-      provideHttpClient(),
-      provideZoneChangeDetection({ eventCoalescing: true }),
-      provideRouter(routes),
-   
-    ]
+    provideHttpClient(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
   };
